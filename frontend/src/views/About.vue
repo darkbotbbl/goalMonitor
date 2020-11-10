748bcaed782f554414cc1e4fdcd92c8afe397a47
darkbot <darkbotbbl@gmail.com>
2020-11-10 16:00:34 +0000

    Added dev and prod dokerfiles to the frontend directory, to enable builds for production and for development

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
     <h1>Let's see if it will change</h1>
+    <p>I have changed it again</p>
+    <p>Mystic: Change, Change, Change</p>
+    <h1>Let's see if it will change</h1>
+    <p>I have changed it again</p>
+    <p>Mystic: Change, Change, Change</p>
+    <h1>Let's see if it will change</h1>
+    <p>I have changed it again</p>
+    <p>Mystic: Change, Change, Change</p>
+    <h1>Let's see if it will change</h1>
+    <p>I have changed it again</p>
+    <p>Mystic: Change, Change, Change</p>
+    <p>I have changed it again</p>
+    <p>Mystic: Change, Change, Change</p>
+    <h1>Let's see if it will change</h1>
+    <p>I have changed it again</p>
+    <p>Mystic: Change, Change, Change</p>
+    <h1>Let's see if it will change</h1>
+    <p>I have changed it again</p>
+    <p>Mystic: Change, Change, Change</p>
+    <h1>Let's see if it will change</h1>
+    <p>I have changed it again</p>
+    <p>Mystic: Change, Change, Change</p> 
   </div>
 </template>
