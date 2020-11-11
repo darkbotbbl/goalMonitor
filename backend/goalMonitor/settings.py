3a56688e31f6ff8cc6f3dd161f1f79344585e9bb
darkbot <darkbotbbl@gmail.com>
2020-11-11 18:44:35 +0000

    Added a lot of extra configuration to the docker files of the backend

--- backend/goalMonitor/settings.py
+++ backend/goalMonitor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&t3mqi^6cy^-f&pf*yjlwqhdvl^=*mtv(649z2v)tei%i#y7%$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['backend', 'localhost', '127.0.0.1', ]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'goalMonitor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        'USER': os.environ.get("SQL_USER", "user"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD", "password"),
+        'HOST': os.environ.get("SQL_HOST", "localhost"),
+        'PORT': os.environ.get("SQL_PORT", "5432"),
     }
 }
 
@@ -116,6 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-STATIC_URL = '/static/'
-
+STATIC_URL="/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -22,6 +22,9 @@
     <p>Mystic: Change, Change, Change</p>
     <h1>Let's see if it will change</h1>
     <p>I have changed it again</p>
+    <p>
+        It is really amazing how this thing is able to change when you reload the page even in docker
+    </p>
     <p>Mystic: Change, Change, Change</p> 
   </div>
 </template>
