be433703edb0f0ae2d5815c8092ccdf26360e1ba
darkbot <darkbotbbl@gmail.com>
2020-12-04 20:44:48 +0000

    Added tests for the forms that will be used to create user accounts

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+
+class CustomUserTests(TestCase):
+
+    def test_user_creation_form_for_creating_a_normal_user(self):
+        user = User.objects.create_user(
+            username="testuser",
+            email="testuser@site.com",
+            password="thepassword",
+        )
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "testuser@site.com"),
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+
+    def test_user_creation_form_for_creating_a_superuser(self):
+        user = User.objects.create_superuser(
+            username="admin",
+            email="admin@site.com",
+            password="adminpassword"
+        )
+        self.assertEqual(user.username, "admin")
+        self.assertEqual(user.email, "admin@site.com")
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
