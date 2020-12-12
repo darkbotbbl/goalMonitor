905e00d76833815bcfad99166221c35747ab95e5
darkbot <darkbotbbl@gmail.com>
2020-12-12 01:08:31 +0000

    Started building the frontend of the website, no interactivity yet, just static - design phase

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,28 @@
 import Vue from 'vue'
-import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars, faMoneyBillWave, faBoxes, faUsersCog, faQuestionCircle, faChartLine, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Using fontawesome
+library.add(faBars, faMoneyBillWave, faBoxes, faUsersCog, faQuestionCircle, faChartLine, faTimes)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
 // Vue Axios
 Vue.use(VueAxios, axios)
 
